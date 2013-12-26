92b9afdf3f8e0f7f89dd12e092845426cb859f19
chaiwat <mynameistecs51@gmail.com>
2013-12-26 11:36:05 +0700

    chaiwat update .../views/admin/student_info.php 26/12/56 11.35

--- application/views/admin/student_info.php
+++ application/views/admin/student_info.php
@@ -36,17 +36,13 @@
 					<div class="modal-body">
 						<form role="form" class="form-inline">
 							<div class="form-group">
-								<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-								<div class="col-sm-10">
-									<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-								</div>
-							</div>
-							<div class="form-group">
-							<div class="col-sm-3">
-									<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-								</div>
-								<div class="col-sm-10">
-									<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+								<span class="control-label">ข้อมูลใช้ล๊อคอิน</span>
+								<div class="controls form-inline">
+									<label for="inputValue">password</label>
+									<input type="email" class="input-small" id="exampleInputEmail2" placeholder="Enter email">
+
+									<label for="inputValue">password</label>
+									<input type="password" class="input-small" placeholder="Value" id="inputValue">
 								</div>
 							</div>
 						</form><!--enform -->
